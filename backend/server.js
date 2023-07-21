cc08d1ea041e680d1e2afaf3fd81c4a5d5003d3a
wajdem <wajdi.abuarqob@gmail.com>
2023-07-21 15:20:23 +0300

    done

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+
+//express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+//connect to db
+
