873dc55144967245d9aed05499a4396d4f5836dc
wajdem <wajdi.abuarqob@gmail.com>
2023-07-28 01:29:09 +0300

    done

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,20 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
 
-const Signup = ()=>{
-   const [email, setEmail] = useState('')
-   const [password, setPassword] = useState('')
+const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const {signup, error, isLoading} = useSignup()
 
-   const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    console.log(email, password);
-   }
+    await signup(email, password)
+  }
 
-   return(
-    <form className='signup' onSubmit={handleSubmit}>
-        <h3>Sign up</h3>
-
-        <label>Email address:</label>
+  return (
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+      
+      <label>Email address:</label>
       <input 
         type="email" 
         onChange={(e) => setEmail(e.target.value)} 
@@ -27,11 +29,10 @@ const Signup = ()=>{
         value={password} 
       />
 
-        <button>Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
-
-   )
-
+  )
 }
 
 export default Signup
