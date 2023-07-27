8d006cfae9406fe7fd01423dcdbaa0a7b451b0f2
wajdem <wajdi.abuarqob@gmail.com>
2023-07-27 23:16:30 +0300

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar';
-
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      <div className="pages">
-        <Routes>
-          <Route
-            path='/'
-            element = {<Home />}
-          />
-        </Routes>
-      </div>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
