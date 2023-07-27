f312a379f1a18c6392a186b3abc8d9447f9553f5
wajdem <wajdi.abuarqob@gmail.com>
2023-07-27 20:53:46 +0300

    done

--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -2,11 +2,13 @@ import { WorkoutsContext } from "../context/workoutContext";
 import { useContext } from "react";
 
 export const useWorkoutsContext = () => {
-    const context = useContext(WorkoutsContext)
+  const context = useContext(WorkoutsContext);
 
-    if (!context) {
-        throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
-    }
+  if (!context) {
+    throw Error(
+      "useWorkoutsContext must be used inside an WorkoutsContextProvider"
+    );
+  }
 
-    return context 
-}
+  return context;
+};
